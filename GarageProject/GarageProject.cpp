785cc8a3d35970f444dbd7d359e6d73fe6db8ba3
Jimmy Hasselberg <hasselberg89@hotmail.com>
2019-12-14 14:42:55 +0100

    started to create classes

--- GarageProject/GarageProject.cpp
+++ GarageProject/GarageProject.cpp
@@ -1,20 +1,101 @@
-// GarageProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+class Garage //representation of the "building" itself. The garage can store x number of vehicles
+{
+	/*
+	ListVehicles som listar samtliga fordon i garaget 
+		• ListTypeOfVehicles som listar samtliga olika typer av  fordon i garaget och hur många av dessa som står i garaget 
+		• AddVehicle som lägger till ett fordon ur garaget  
+		• RemoveVehicle som tar bort fordon ur garaget  
+		• Ha en max - gräns som krävs som inparameter vid instansieringen av ett nytt garage  
+		• SearchVehicle som söker efter ett specifikt fordon via REG - NR och skriver ut det om det finns, annars inte.
+		• Möjlighet att söka efter ett flertal fordon på ett flertal valfria variabler.
+	*/
+};
+
+class Vehicle //headclass of vehicles such as cars,bikes trucks etc.
+{
+	/*
+	•  Vehicle innehåller samtliga variabler som ska finnas i samtliga fordonstyper.EX.REG - NR, Färg, Antal hjul.Och andra variabler ni kan komma på.
+		• Det måste minst finnas följande subklasser : 
+			o Bicycle 
+			o Motorcycle  
+			o Car
+			o Bus  
+			o Truck
+		• Dessa skall innehålla minst två egna attribut
+	*/
+private:
+
+protected:
+	string manufacturer;
+	string model;
+	string registrationNumber;
+	string colour;
+	short int numberOfWheels;
+
+public:	
+	Vehicle(string a, string b, string c, string d, short int i) : manufacturer(a), model(b), registrationNumber(c), colour(d), numberOfWheels(i) 
+	{
+	}
+
+	virtual void output() = 0;
+};
+
+class Car : public Vehicle
+{
+public:
+	
+	Car(string a, string b, string c, string d, short int i) : Vehicle(a,b,c,d,i)
+	{
+	}
+
+	void output()
+	{
+		cout << "Manufacturer: " << this->manufacturer << " \nModel: " << this->model << "\nRegistrationnumber: " << this->registrationNumber << "\nColour: " << this->colour << "\nNumber of wheels: " << this->numberOfWheels << "\n*************" << endl;
+	}
+};
+
+class Motorcycle : public Vehicle
+{
+public:
+
+	Motorcycle(string a, string b, string c, string d, short int i) : Vehicle(a, b, c, d, i)
+	{
+	}
+
+	void output()
+	{
+		cout << "Manufacturer: " << this->manufacturer << " \nModel: " << this->model << "\nRegistrationnumber: " << this->registrationNumber << "\nColour: " << this->colour << "\nNumber of wheels: " << this->numberOfWheels << "\n*************" << endl;
+	}
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*
+	• Ett textbaserat UI för en konsolapplikation 
+		o Det skall gå att navigera till SAMTLIG funktionalitet från garage via UI : t 
+		o Det skall gå att SKAPA ett Garage i UI : t.
+		o Det skall gå att avsluta applikationen från UI : t
+	*/
+	Vehicle* ptrCarOne = new Car("Volvo", "V70" ,"ABC123", "Red", 4);
+	Vehicle* ptrCarTwo = new Car("Audi", "A4", "AFS143", "Silver", 4);
+	Vehicle* ptrBikeOne = new Motorcycle("Kawasaki", "ER6n", "HML584", "Black", 2);
+
+	ptrCarOne->output();
+	ptrCarTwo->output();
+	ptrBikeOne->output();
+
+
+	delete ptrBikeOne;
+	delete ptrCarOne;
+	delete ptrCarTwo;
+
+	
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
